f2d04d788ec3621d9e5352b43db09b61b9a29c43
Akhil <akhilu5042@gmail.com>
2022-01-21 23:12:51 +0530

    Program.cs

---
+++ MyLinkedListProblems/LinkedList.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLinkedListProblems
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+        public Node RemoveFirstNode()
+        {
+            if (this.head == null)
+            {
+                return null;
+            }
+            this.head = this.head.next;
+            return this.head;
+        }
+        public Node RemoveLastNode()
+        {
+            if (head == null)
+            {
+                return null;
+            }
+            if (head.next == null)
+            {
+                return null;
+            }
+            Node NewNode = head.next;
+            while (NewNode.next.next != null)
+            {
+                NewNode = NewNode.next;
+            }
+            NewNode.next = null;
+            return head;
+
+        }
+        public int Search(int value)
+        {
+            Node node = this.head;
+            int count = 0;
+            while (node != null)
+            {
+
+                if (node.data == value)
+                {
+                    return count;
+                }
+                node = node.next;
+                count++;
+            }
+            return count;
+        }
+        public Node InsertAtParticularPosition(int position, int data)
+        {
+            Node newestNode = new Node(data);
+            if (this.head == null)
+            {
+                return newestNode;
+            }
+            if (position == 0)
+            {
+                newestNode.next = this.head;
+                this.head = newestNode;
+                return this.head;
+            }
+            Node prev = null;
+            Node current = this.head;
+            int count = 0;
+            while (current != null && count < position)
+            {
+                prev = current;
+                current = current.next;
+                count++;
+            }
+            newestNode.next = prev.next;
+            prev.next = newestNode;
+            return this.head;
+        }
+        public void AddInReverseOrder(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                head = newNode;
+                head.next = temp;
+            }
+        }
+        internal void DeleteNodeAtParticularPosition(int position)
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            Node temp = this.head;
+            if (position == 0)
+            {
+                this.head = temp.next;
+                return;
+            }
+            for (int i = 0; temp != null && i < position - 1; i++)
+            {
+                temp = temp.next;
+
+            }
+            if (temp == null || temp.next == null)
+            {
+                return;
+            }
+            Node next = temp.next.next;
+            temp.next = next;
+            Size();
+        }
+        internal void Size()
+        {
+            Node temp = this.head;
+            int count = 0;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+                count++;
+            }
+            Console.WriteLine("\nLength of LinkedList is :-" + " " + count);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyLinkedListProblems;
+using System.Collections.Generic;
+
+
+
+
+
+
+
+
+bool flag = true;
+while (flag)
+{
+    Console.WriteLine("\nEnter the program number which is to be executed \n 1.UC-1, \n 2.UC-2, \n 3.UC-3, \n 4.UC-4, \n 5.UC-5, \n 6.UC-6, \n 7.UC-7, \n 8.UC-8, \n 9.UC-9, \n 10.UC-10, \n 11.Exit");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            break;
+        case 2:
+            LinkedList list1 = new LinkedList();
+            list1.Add(56);
+            list1.Add(30);
+            list1.Add(70);
+            list1.Display();
+            break;
+        case 3:
+            LinkedList link = new LinkedList();
+            link.Add(56);
+            link.Add(30);
+            link.Add(70);
+            link.InsertAtParticularPosition(2, 30);
+            link.Display();
+            break;
+        case 4:
+            LinkedList link1 = new LinkedList();
+            link1.Add(56);
+            link1.Add(30);
+            link1.Add(70);
+            link1.Display();
+            break;
+        case 5:
+            LinkedList link2 = new LinkedList();
+            link2.Add(56);
+            link2.Add(30);
+            link2.Add(70);
+            link2.RemoveFirstNode();
+            Console.WriteLine("After removing is : ");
+            link2.Display();
+            break;
+        case 6:
+            LinkedList link3 = new LinkedList();
+            link3.Add(56);
+            link3.Add(30);
+            link3.Add(70);
+            link3.RemoveLastNode();
+            Console.WriteLine("After removing is : ");
+            link3.Display();
+            break;
+        case 7:
+            LinkedList link4 = new LinkedList();
+            link4.Add(56);
+            link4.Add(30);
+            link4.Add(70);
+            int search = link4.Search(30);
+            Console.WriteLine(search);
+            break;
+        case 8:
+            LinkedList link5 = new LinkedList();
+            link5.Add(56);
+            link5.Add(30);
+            link5.Add(70);
+            int position = link5.Search(30);
+            position++;
+            link5.InsertAtParticularPosition(position, 40);
+            link5.Display();
+            break;
+        case 9:
+            LinkedList link6 = new LinkedList();
+            link6.Add(56);
+            link6.Add(30);
+            link6.Add(70);
+            link6.Display();
+            int position1 = link6.Search(40);
+            link6.DeleteNodeAtParticularPosition(position1);
+            break;
+        case 10:
+
+            break;
+        case 11:
+            flag = false;
+            break;
+    }
+}
